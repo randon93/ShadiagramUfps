0bc86bf9cccd37e2a3e8fd8aa6ae366115555487
randon93 <32597818+randon93@users.noreply.github.com>
2019-04-22 21:00:40 -0500

    Update plantilla.php

--- VIEWS/PLANTILLA/plantilla.php
+++ VIEWS/PLANTILLA/plantilla.php
@@ -3,13 +3,13 @@
 <head>
   <meta charset="utf-8">
   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-  <link rel="stylesheet" href="public/bootstrap4\css\myStilo.css">
-  <link rel="stylesheet" type="text/css" href="public/bootstrap4\css\ihover.css" />
-  <link rel="stylesheet" type="text/css" href="public/bootstrap4\css\ihover.min.css" />
+  <link rel="stylesheet" href="<?php echo constant('URL'); ?>public/bootstrap4\css\myStilo.css">
+  <link rel="stylesheet" type="text/css" href="<?php echo constant('URL'); ?>public/bootstrap4\css\ihover.css" />
+  <link rel="stylesheet" type="text/css" href="<?php echo constant('URL'); ?>public/bootstrap4\css\ihover.min.css" />
   <script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.7.8/angular.min.js"></script>
   <script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.7.8/angular-route.js"></script>
   <script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.7.8/angular-route.min.js"></script>
-  <script src="public/bootstrap4/js/app.js"></script>
+  <script src="<?php echo constant('URL'); ?>public/bootstrap4/js/app.js"></script>
 
   <title>Perfil de Usuario</title>
 </head>
@@ -51,10 +51,7 @@
    <!-- **************************************************************** -->
 
     <div id="view">
-      <?php
-
-       require ('views/'.constant('VISTA').'.php');
-        ?>
+      <?php require ('views/'.constant('VISTA').'.php')  ?>
     </div>
 
    <!-- **************************************************************** -->
@@ -66,8 +63,11 @@
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
-	  <script src="public/bootstrap4/js/jquery-3.1.1.min.js"></script>
-    <script src="public/bootstrap4/js/main.js"></script>
+	  <script src="<?php echo constant('URL'); ?>ublic/bootstrap4/js/jquery-3.1.1.min.js"></script>
+    <script src="<?php echo constant('URL'); ?>ublic/bootstrap4/js/main.js"></script>
+    <script type="text/javascript">
+    $('#mostrarmodal').modal('show');
+    </script>
 
 </body>
 
