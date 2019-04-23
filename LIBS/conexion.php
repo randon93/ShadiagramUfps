bb9f37e514380a4ae9f6ee4188bd1648de854a82
randon93 <32597818+randon93@users.noreply.github.com>
2019-04-22 21:00:11 -0500

    Update conexion.php

--- LIBS/conexion.php
+++ LIBS/conexion.php
@@ -17,8 +17,8 @@ class Conexion {
       try {
 
           $optiones = [
-            PDO::ATTR_ARRMODE                  => PDO::ERRMODE_EXCEPTION,
-            PDO::ATTR_EMULATE_PREPARES  => false,
+            PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+            PDO::ATTR_EMULATE_PREPARES  => false
           ];
 
           $this->pdo = new PDO('mysql:host=127.0.0.1;dbname=' . $this->bd , $this->usuarioBd , $this->pasword, $optiones);
@@ -38,7 +38,7 @@ class Conexion {
 
   }
 
-  private function cerrarCon(){
+  public function cerrarCon(){
     $this->pdo = null;
   }
 
@@ -54,18 +54,23 @@ class Conexion {
 
     if(count($columna) == count($parametro)){
 
+        $array = [];
         if (!empty($tabla) ) { // si $tabla trae un parametro se busca! si esta vacio retorna NULL.
-          if (count($parametro) == 0) {;// si el parametro esta vacio se muestra toda la info de la tabla
+          if (count($parametro) == 0) {// si el parametro esta vacio se muestra toda la info de la tabla
             $sql = "SELECT * FROM " . $tabla;
           }else {
             if (count($columna) > 1 ) {
               $sql = "SELECT * FROM " . $tabla . " WHERE " . $columna[0] ." = " . $parametro[0] . $this->aggAnd($parametro, $columna);
             }
             $sql = "SELECT * FROM " . $tabla . " WHERE " . $columna[0] ." = " . $parametro[0] ;
-            echo $sql;
+
             $con = $this->conectar()->prepare($sql);
             $respuesta = $con -> execute();
-          //  var_dump $respuesta;
+            if ($respuesta->fetchColumn() > 0) {
+              foreach ($respuesta as $res) {
+                  array_puesh($array, ['nombre'=>$res["nombre"], 'email'=>$res["email"],]);
+              }
+            }
           }
         }else {
           $sql = "nada";
@@ -78,7 +83,19 @@ class Conexion {
     //  return $sql;
   }
 
-
+  /** //////////////////////////////////////////////  */
+            /**  metodo en el cual nos retona
+        *un TRUE si el usuario ya existe en la BD*/
+  /** //////////////////////////////////////////////  */
+  public function verificarEx($email){
+      $con = $this->conectar();
+      $exito = $con->prepare("SELECT COUNT(*) FROM usuario WHERE email = ?");
+      $exito->execute(array($email));
+      if ($exito->fetchColumn() > 0) {
+        return true;
+      }
+      return false;
+  }
 
 }//clase
 
