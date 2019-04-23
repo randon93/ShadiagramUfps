3e2a897a9e4571f5483e7320af3612d15993e0e7
randon93 <32597818+randon93@users.noreply.github.com>
2019-04-22 20:59:48 -0500

    Create loginController.php

---
+++ CONTROLLERS/loginController.php
@@ -0,0 +1,46 @@
+<?php
+/**
+ *
+ */
+
+class Login extends controller{
+
+  function __construct()  {
+    parent::__construct();
+
+
+  }
+  /** //////////////////////////////////////////////  */
+      /** METODO RENDERISAR VISTA DEL CONTROLADOR*/
+  /** //////////////////////////////////////////////  */
+
+  function render($r){
+    $this->view->render($r . '/index');
+  }
+
+  /** //////////////////////////////////////////////  */
+            /** METODO AGREGAR USUARIO*/
+  /** //////////////////////////////////////////////  */
+
+  function registrar(){
+
+    $alias = $_POST['alias'];
+    $nombre    = $_POST['nombre'];
+    $email  = $_POST['email'];
+    $password = $_POST['password'];
+
+    $exito = $this->model->agg(["nombre" => $nombre, "alias" => $alias, "email" => $email, "password" => $password ]);
+
+    if ($exito) {
+      echo constant('SESION');
+      defined('SESION',$alias);
+      echo constant('SESION');
+      header("location:".constant('URL')."perfil");
+      //$this->render('perfil');
+    }else {
+      $this->render('error');
+    }
+  }
+
+}
+ ?>
