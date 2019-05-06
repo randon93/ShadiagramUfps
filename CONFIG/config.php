61d18aa31699a032bf71a02299755660a5ba171d
randon93 <32597818+randon93@users.noreply.github.com>
2019-05-06 08:01:01 -0500

    Update config.php

--- CONFIG/config.php
+++ CONFIG/config.php
@@ -1,5 +1,5 @@
 <?php
-  define ('bd','SHADIAGRAM'); //NOMBRE DE LA BASE DE DATOS
+  define ('bd','SHADIAGRAMUFPS'); //NOMBRE DE LA BASE DE DATOS
   define ('usuarioBd','root'); //Usuario DE LA BASE DE DATOS
   define ('password',''); // CONTRASEÑA DE LA BASE E DATOS
   define ('host','127.0.0.1'); // HOST DEL SERVIDOR
